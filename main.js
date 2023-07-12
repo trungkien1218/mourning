b07cd2197958eff644a60ea475d03a15acdf4f31
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-07-12 23:44:34 +0700

    product chuưa dc

--- main.js
+++ main.js
@@ -17,21 +17,26 @@ let get_flower ={
 } 
 async function render_flower(params) {
     for (let flower of params){
-        let {name, price, image} = flower;
+        let {name, price, image, id} = flower;
         let div = document.createElement('div');
-        div.classList.add('product');
+        div.classList.add('item');
         div.innerHTML = `
-        <div class="product">
-        <a href="#">
+        
+        <a href="product-detail.html">
            <div class="image" style="background-image: url(${image});"></div>
            <p class="name">${name}</p>
            <p class="price">${price}</p>
         </a>
-     </div>
+    
         `;
         document.querySelector('.sanpham').appendChild(div);
+        div.addEventListener('click', function(){
+            localStorage.setItem('prd_id', id);
+
+        })
     }
 }
+
  async function fetch_data(params){
     if (!params){
         alert('Không tồn tại request');
@@ -42,14 +47,72 @@ async function render_flower(params) {
         let res = await fetch(api_url + end_point, {
             method: method
         });
-
+        
         let data = await res.json();
         await callback(data)
     }
     catch(error) {
         console.log(error)
     }
-
-
 }
 fetch_data(get_flower);
+
+
+
+///chi tiết sản phẩm
+
+let get_flower_by_id ={
+    api_url: api_url,
+    end_point: end_point. flower + 'prd_id' ,
+    method: 'GET',
+     async callback(params){
+        await render_flower_prd(params)
+   
+    }
+} 
+
+let prd_id= '';
+if (localStorage.getItem('prd_id')) prd_id = localStorage.getItem('prd_id');
+
+
+async function render_flower_prd(params) {
+    for (let flower of params){
+        let {name, price, image, id} = flower;
+        let div = document.createElement('div');
+        div.classList.add('item-prd');
+        div.innerHTML = `
+        
+        <div class="image-prd">
+            ${image}
+        </div>
+        <div class="product">
+            <p class="name-pr">${name}</p>
+            <p class="price-pr">${price}</p>
+
+        </div>
+ 
+        
+       
+    
+        `;
+        document.querySelector('.grid').appendChild(div);
+       
+    }
+}
+fetch_data(get_flower_by_id);
+
+
+//if( prd_id === '') return false;
+/*
+//let prd_id= '';
+//if (localStorage.getItem('prd_id')) prd_id = localStorage.getItem('prd_id');
+//if( prd_id === '') return false;
+async function fetch_data_by_id(){
+    let res = await fetch('https://6487093ebeba6297278fbab7.mockapi.io/flower' + localStorage.getItem('prd_id));
+    let data = await res.json();
+    await console.log(data);
+
+}
+fetch_data_by_id();
+*/
+
